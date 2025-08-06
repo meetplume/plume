68e8581914c520705f82ac6a4eef3660b2b76768
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-08-07 00:07:29 +0200

    feat: add account dropdown with authentication actions in navigation
    - Introduce account-related dropdown component in the main navigation bar.
    - Provide "Log in," "Register," and "Logout" options based on authentication state.
    - Update language files with new translations for account actions.

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,7 +2,7 @@
     use App\Support\Icons;
     use App\Enums\Analytics;
     use App\Enums\MainPages;
-    use App\Enums\SiteSettings;
+    use App\Enums\SiteSettings;use Filament\Facades\Filament;
 @endphp
 
 <nav {{ $attributes->class('flex items-center gap-6 md:gap-8 justify-between') }}>
@@ -45,6 +45,54 @@
                 {{ $name }}
             </a>
         @endforeach
+
+        <x-dropdown>
+            <x-slot:btn
+                data-pan="{{ Analytics::MAIN_MENU->value }}-account"
+                class="transition-colors hover:text-primary-600 dark:hover:text-primary-500 cursor-pointer"
+            >
+                {!! Icons::getHeroicon(
+                    name: 'user',
+                    isOutlined: true,
+                    class: 'mx-auto size-6'
+                ) !!}
+                @auth
+                    {{ str(auth()->user()->name)->limit(10) }}
+                @else
+                    {{ __('Account') }}
+                @endauth
+            </x-slot>
+
+            <x-slot:items class="mt-4">
+                @auth
+                    <form method="POST" action="{{ Filament::getPanel('user')->getLogoutUrl() }}" class="inline">
+                        @csrf
+                        <x-dropdown.item
+                            data-pan="{{ Analytics::MAIN_MENU->value }}-logout"
+                            type="submit"
+                        >
+                            {!! svg('heroicon-c-arrow-right-start-on-rectangle', 'size-4')->toHtml() !!}
+                            {{ __("Log out") }}
+                        </x-dropdown.item>
+                    </form>
+                @else
+                    <x-dropdown.item
+                        data-pan="{{ Analytics::MAIN_MENU->value }}-login"
+                        href="{{ Filament::getPanel('user')->getLoginUrl() }}"
+                    >
+                        {!! svg('heroicon-c-arrow-right-end-on-rectangle', 'size-4')->toHtml() !!}
+                        {{ __('Log in') }}
+                    </x-dropdown.item>
+                    <x-dropdown.item
+                        data-pan="{{ Analytics::MAIN_MENU->value }}-register"
+                        href="{{ Filament::getPanel('user')->getRegistrationUrl() }}"
+                    >
+                        {!! svg('heroicon-c-user-plus', 'size-4')->toHtml() !!}
+                        {{ __('Register') }}
+                    </x-dropdown.item>
+                @endauth
+            </x-slot:items>
+        </x-dropdown>
 
         <x-dropdown>
             <x-slot:btn
