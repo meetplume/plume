df7e675ce68a9a2e0d1b028c3d8b90ca9dfd3f31
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-08-18 23:49:02 +0200

    make section title level customizable

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,10 +1,10 @@
 <section {{ $attributes->class('container scroll-mt-4') }}>
     @if (! empty($title))
-        <h1 @class([
+        <h{{ (string) ($attributes['level'] ?? '2') }} @class([
             'font-bold tracking-widest text-center text-black dark:text-white uppercase text-balance mb-8',
         ])>
             {!! $title !!}
-        </h1>
+        </h{{ (string) ($attributes['level'] ?? '2') }}>
     @endif
 
     {{ $slot }}
