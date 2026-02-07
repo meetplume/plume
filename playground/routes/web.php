191cab0ef104047be05285c39a04715eef4357d9
Patricio <ijpatricio@gmail.com>
2026-02-07 11:32:38 +0000

    wip

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -7,14 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/movies', function () {
     return Page::render('page1', [
         'title' => 'Iconic Movies',
         'message' => 'This displays a page displaying iconic movies. It is rendered using the Page class from the Plume package.',
     ]);
 });
-
 
 Route::get('/tv-shows', function () {
     return Page::render('page2', [
