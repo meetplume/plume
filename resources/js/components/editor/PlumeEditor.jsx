5c5b25f28ffc654f5afcd970171d405f9949bf72
Patricio <ijpatricio@gmail.com>
2026-02-07 07:06:07 +0000

    Add Tiptap Editor

---
+++ resources/js/components/editor/PlumeEditor.jsx
@@ -0,0 +1,29 @@
+import { EditorContent, EditorContext, useEditor } from '@tiptap/react';
+import { BubbleMenu, FloatingMenu } from '@tiptap/react/menus';
+import StarterKit from '@tiptap/starter-kit';
+import { useMemo } from 'react';
+
+function TiptapExperiment() {
+    const editor = useEditor({
+        extensions: [StarterKit], // define your extension array
+        content: '<p>Hello World!</p>', // initial content
+        editorProps: {
+            attributes: {
+                class: 'prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 p-2',
+            },
+        },
+    });
+
+    // Memoize the provider value to avoid unnecessary re-renders
+    const providerValue = useMemo(() => ({ editor }), [editor]);
+
+    return (
+        <EditorContext.Provider value={providerValue}>
+            <EditorContent editor={editor} />
+            <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+            <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+        </EditorContext.Provider>
+    );
+}
+
+export default TiptapExperiment
--- resources/js/pages/documentation.tsx
+++ resources/js/pages/documentation.tsx
@@ -1,5 +1,11 @@
+import TiptapExperiment from '../components/editor/PlumeEditor';
+
 export default function App({ appName }: { appName: string }) {
+    return (
+        <div>
+            <div className="bg-blue-500"> Foo bar {appName}</div>
 
-    return <div className="bg-blue-500"> Foo bar {appName}</div>;
-
+            <TiptapExperiment />
+        </div>
+    );
 }
