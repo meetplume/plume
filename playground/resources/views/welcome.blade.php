ec1d99e54489234d9910f133d35a74cd0bab0035
Patricio <ijpatricio@gmail.com>
2026-02-07 12:30:15 +0000

    wip

--- playground/resources/views/welcome.blade.php
+++ playground/resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
         <div class="text-center">
             <h1 class="text-4xl font-bold text-gray-900 mb-8">Plume Playground</h1>
             <div class="flex gap-4 justify-center">
-                <a href="/movies" class="px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-gray-700 transition">Movies</a>
-                <a href="/tv-shows" class="px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-gray-700 transition">TV Shows</a>
+                <a href="/movies" target="_blank" class="px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-gray-700 transition">Movies</a>
+                <a href="/tv-shows" target="_blank" class="px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-gray-700 transition">TV Shows</a>
             </div>
         </div>
     </body>
