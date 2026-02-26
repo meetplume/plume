6b0dbaf407aff209eb9a53ba41de0a74f6f4f2a0
CHARLIE ETIENNE <ce@web-nancy.fr>
2026-02-26 22:45:24 +0100

    change copy icon + make radius customizable

--- resources/js/components/plume/markdown-renderer.tsx
+++ resources/js/components/plume/markdown-renderer.tsx
@@ -1,4 +1,5 @@
 import { normalizeCalloutSyntax, remarkCallouts } from '@/lib/remark-callouts';
+import { createInlineSvgUrl } from '@expressive-code/core';
 import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
 import { usePage } from '@inertiajs/react';
 import type { Code, Root } from 'mdast';
@@ -84,6 +85,13 @@ export function MarkdownRenderer({ page, className }: MarkdownRendererProps) {
                 .use(rehypeExpressiveCode, {
                     themes: [codeThemeDark as ThemeObjectOrShikiThemeName, codeThemeLight as ThemeObjectOrShikiThemeName],
                     plugins: [pluginCollapsibleSections()],
+                    styleOverrides: {
+                        frames: {
+                            copyIcon: createInlineSvgUrl(
+                                `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.25 5.25H7.25C6.14543 5.25 5.25 6.14543 5.25 7.25V14.25C5.25 15.3546 6.14543 16.25 7.25 16.25H14.25C15.3546 16.25 16.25 15.3546 16.25 14.25V7.25C16.25 6.14543 15.3546 5.25 14.25 5.25Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M2.80103 11.998L1.77203 5.07397C1.61003 3.98097 2.36403 2.96397 3.45603 2.80197L10.38 1.77297C11.313 1.63397 12.19 2.16297 12.528 3.00097" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
+                            ),
+                        },
+                    },
                 })
                 .use(rehypeRaw)
                 .use(rehypeSlug)
